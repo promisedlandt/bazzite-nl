eb137064061c586dd0b2ce920b0a2b30f044fa9d
Nils Landt <nils@landt.email>
2026-01-17 15:04:31 +0100

    make directories for guix and nix, install wezterm

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,15 +10,19 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux
 
 # Use a COPR Example:
 #
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
+dnf5 -y copr enable ublue-os/staging
+dnf5 -y copr enable wezfurlong/wezterm-nightly
+dnf5 -y install wezterm
 # Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+
+mkdir /gnu
+mkdir /nix
